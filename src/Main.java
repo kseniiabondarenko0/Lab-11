24392d5fbcacf8cf219cc522ffcc4a36415408e1
Your Name <bondarenkoksenia2007@gmail.com>
2026-01-12 10:53:05 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+
+/**
+ * @author Kseniia Bondarenko
+ * @version 1.0
+ * @since 12.01.2026
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("src/Introduction to Programming 2025 Data.csv");
+
+    }
+}
